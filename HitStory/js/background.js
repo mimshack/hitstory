48deadd643eded69c13f4c8529ff92ba63ba5387
avishayhajbi <avishayhajbi@gmail.com>
2014-05-20 02:30:14 +0300

    update db status
    db is almost finish.
    
    @zoharsalah you need to update your uploaded icons to png. thanks.

--- HitStory/js/background.js
+++ HitStory/js/background.js
@@ -1,11 +1,48 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        chrome.tabs.captureVisibleTab(
-            null,
-            {},
-            function(dataUrl){
-                sendResponse({imgSrc:dataUrl});
-            }); //remember that captureVisibleTab() is a statement
-        return true;
-    }
-);
+var newTabIsOpened= false;
+
+// capture page
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	chrome.tabs.captureVisibleTab(null, {}, function(dataUrl) {
+		sendResponse({
+			imgSrc : dataUrl,
+			tab : newTabIsOpened
+		});
+	});
+	//remember that captureVisibleTab() is a statement
+	return true;
+});
+
+
+/************ DON'T CHANGE THE FUNCTION OREDER ************/
+
+// tabs change to another
+chrome.tabs.onActiveChanged.addListener(function(){
+	console.log("**** tab wad unselected ****");
+	newTabIsOpened = false;
+	var notification = webkitNotifications.createNotification( '48.png',	 'Tab Alert',	 'tab is changed' );
+	    //notification.show();
+});
+
+// tabs replaced -- refreshed
+chrome.tabs.onReplaced.addListener(function(){
+	console.log("**** tab was replaced ****");
+	newTabIsOpened = false;
+	var notification = webkitNotifications.createNotification( '48.png',	 'Tab Alert',	 'tab is replaced' );
+	    //notification.show();
+});
+
+// on tab created
+chrome.tabs.onCreated.addListener(function(){
+	console.log("**** new tab has been created ****");
+	newTabIsOpened = true;
+	var notification = webkitNotifications.createNotification( '48.png',	 'Tab Alert',	 'new tab created' );
+	    //notification.show();
+});
+
+// on tab onRemoved
+chrome.tabs.onRemoved.addListener(function(){
+	console.log("**** tab was removed ****");
+	newTabIsOpened = false;
+	var notification = webkitNotifications.createNotification( '48.png',	 'Tab Alert',	 'tab was removed' );
+	    //notification.show();
+});
--- HitStory/js/saveHistory.js
+++ HitStory/js/saveHistory.js
@@ -1,56 +1,78 @@
 // takes 6 params returns 1 json type class
-function createTabClass(created,active_time,closed,title,url,is_root,children,image_url){    
-    var tab_class = {
-        created :created, // check javasacript Date()
-        active_time: active_time,//chrome events onActivated
-        closed: closed, //chrome events onRemoved
-        title:title, //get from $('title').html()
-        url:url, //document.location.href
-        is_root:"is?", //if this is opend from google, or blank new tab
-        children:"child", //add array of the childrens id's 
-        img_url:image_url, //chrome events captureVisibleTab
-    };
-    return tab_class;
+function createTabClass(created, active_time, closed, title, url, is_root, children, image_url) {
+	var tab_class = {
+		created : created, // check javasacript Date()
+		active_time : active_time, //chrome events onActivated
+		closed : closed, //chrome events onRemoved
+		title : title, //get from $('title').html()
+		url : url, //document.location.href
+		is_root : is_root, //if this is opend from google, or blank new tab
+		children : children, //add array of the childrens id's
+		img_url : image_url, //chrome events captureVisibleTab
+	};
+	return tab_class;
 }
 
 /* Using underscore js to find stuff - http://underscorejs.org/ */
 //var where = _.where(page_data, {name: "andy", price: 50});
 //var where = _.where(page_data, {id: 1});
 //GLOBAL variables
-var page_data= [];
-var image_url=null;
-function addToStorage(add_data){
-    page_data.push(add_data);
-    chrome.storage.local.set({'page_data': page_data});
-    //chrome.storage.sync();
+var page_data = [];
+var children = [];
+var image_url = null;
+var newTabIsOpened = false;
+var checkGlobal="i am global";
+function addToStorage(add_data) {
+	page_data.push(add_data);
+	chrome.storage.local.set({
+		'page_data' : page_data
+	});
+	//chrome.storage.sync();
 }
 
-function getStorage() { 
-    chrome.storage.local.get('page_data', function (result) {
-        page_data = result.page_data;
-        setTimeout(function() {}, 5000);});
-        return page_data;
-} 
-
-function displayStorage(){
-        console.table(page_data);
-        console.log( page_data);
+function getStorage() {
+	chrome.storage.local.get('page_data', function(result) {
+		page_data = result.page_data;
+		setTimeout(function() {
+		}, 5000);
+	});
+	return page_data;
 }
-function capture_image_url(){
-	chrome.runtime.sendMessage({msg: "capture"}, function(response) {
-	  	console.log(response.imgSrc);
-	  	image_url=response.imgSrc;
-	  	getStorage();
-	  	saveData();
+
+function displayStorage() {
+	console.table(page_data);
+	console.log(page_data);
+}
+
+function capture_image_url() {
+	chrome.runtime.sendMessage({
+		msg : "capture"
+	}, function(response) {
+		console.log(response.imgSrc);
+		image_url = response.imgSrc;
+		console.log(response.tab);
+		newTabIsOpened = response.tab;
+		getStorage();
+		calculateTimes();
+		saveData();
 	});
 }
-function saveData(){
-    var add_data = createTabClass(new Date().getTime(),'active_time','closed',document.title,window.location.href,'is_root','children',image_url);
-    page_data.push(add_data);//the DB is still empty
-    chrome.storage.local.set({'page_data': page_data}, function (result){
-    	addToStorage(add_data); // another call to DB and now it success
-    	displayStorage(); 
-    });
+
+function calculateTimes(){
+	// here we will calculate 'active time' children and more...
+}
+
+function saveData() {
+	var add_data = createTabClass(new Date().getTime(), "active_time", 'closed', document.title, window.location.href, newTabIsOpened, children, image_url);
+	page_data.push(add_data);
+	//the DB is still empty
+	chrome.storage.local.set({
+		'page_data' : page_data
+	}, function(result) {
+		addToStorage(add_data);
+		// another call to DB and now it success
+		displayStorage();
+	});
 }
 
 // look at createTabClass() to know how to get the correct vars
@@ -58,92 +80,72 @@ $(document).ready(function() {
 	capture_image_url();
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
-var database = {
-	size : 0
-};
-function prePageLoad() {
+ var database = {
+ size : 0
+ };
+ function prePageLoad() {
 
-	chrome.storage.local.get('', function(result) {
-		if (result.database == null || result.database == undefined)
-			database.size = 1;
-		else if (result.database.size == 0) {
-			database.size += result.database.size;
-		} else
-			database.size = 1;
-	});
+ chrome.storage.local.get('', function(result) {
+ if (result.database == null || result.database == undefined)
+ database.size = 1;
+ else if (result.database.size == 0) {
+ database.size += result.database.size;
+ } else
+ database.size = 1;
+ });
 
-	console.log("get db size " + database.size);
+ console.log("get db size " + database.size);
 
-	saveHistory();
-}
+ saveHistory();
+ }
 
-function changeDBsize() {
-	database.size += 1;
-	chrome.storage.local.set({
-		database : database
-	}, function() {
-		console.log("DB SIZE SAVED " + database.size);
-	});
-}
+ function changeDBsize() {
+ database.size += 1;
+ chrome.storage.local.set({
+ database : database
+ }, function() {
+ console.log("DB SIZE SAVED " + database.size);
+ });
+ }
 
-function displayDataBase() {
-	// read from data base
-	for (var i = 1; i <= database.size; i++) {
-		var obj = "key"+i.toString();
-		chrome.storage.local.get(obj, function(result) {
-			console.log("read dataBase: ");
-			if (result.obj != null) {
-				console.log(result.obj.url);
-				console.log(result.obj.name);
-			}
-		});
-	}
-}
+ function displayDataBase() {
+ // read from data base
+ for (var i = 1; i <= database.size; i++) {
+ var obj = "key"+i.toString();
+ chrome.storage.local.get(obj, function(result) {
+ console.log("read dataBase: ");
+ if (result.obj != null) {
+ console.log(result.obj.url);
+ console.log(result.obj.name);
+ }
+ });
+ }
+ }
 
-// store data in chrome DB
-function saveHistory() {
-	var jsonName = "key"+database.size.toString();
-	console.log("jsonName " + jsonName);
-	// save json type
-	var jsonName = {
-		url : "avishay",
-		name : "hajbi"
-	};
+ // store data in chrome DB
+ function saveHistory() {
+ var jsonName = "key"+database.size.toString();
+ console.log("jsonName " + jsonName);
+ // save json type
+ var jsonName = {
+ url : "avishay",
+ name : "hajbi"
+ };
 
-	// store into data base
-	chrome.storage.local.set({
-		jsonName : jsonName
-	}, function() {
-		console.log("jason SAVED");
-	});
+ // store into data base
+ chrome.storage.local.set({
+ jsonName : jsonName
+ }, function() {
+ console.log("jason SAVED");
+ });
 
-	changeDBsize();
+ changeDBsize();
 
-	displayDataBase();
+ displayDataBase();
 
-}
-*/
+ }
+ */
 
 /*	// display DB size in MB
  var storageSize = 1;
